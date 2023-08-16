3674f75663ebbfd461a33b9259185ed797085140
m-eminsahin <104202548+m-eminsahin@users.noreply.github.com>
2023-08-16 19:26:11 +0300

    GuessingGame

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            string secretWord = "Hardal";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 10;
+            bool outOfGuesses = false;
+
+
+            while(guess != secretWord && !outOfGuesses)
+            {
+                if(guessCount < guessLimit)
+                {
+                    Console.WriteLine("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                { 
+                   outOfGuesses = true;
+                }
+                              
+            }
+            if (outOfGuesses)
+            {
+                Console.WriteLine("You Lose!");
+            }
+            else
+            {
+                Console.WriteLine("You Win!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
